bad6f88cdc22ce42e98774173012f6fdf022f1ee
Fabiola BELET <fabiola.belet@gmail.com>
2018-11-09 10:41:32 -0500

    Design 2

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,8 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 
 class AppController extends AbstractController
 {
   
+    function base(CategoryRepository $repoCat){
+        $categories = $repoCat->findAll();
+        return $this->render('base.html.twig', [
+            'categories' => $categories
+        ]);
+
+    }
+
 }
