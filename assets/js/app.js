7ade4f228c5278c0acb56a74b8e1ac2ab597b4af
Fabiola BELET <fabiola.belet@gmail.com>
2018-11-14 17:46:29 -0500

    Ok caroussel

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,10 @@
 require('../css/app.css');
 require('../css/global.scss');
 
-const $ = require('jquery');
+
+import $ from 'jquery';
+
+
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-
-
